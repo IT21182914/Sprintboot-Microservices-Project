bfbd9f03d1cecc1300d389c64798d439a3fac38d
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-04-04 15:27:00 +0530

    Enable Feign Clients

--- order-service/src/main/java/com/techie/microservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/techie/microservices/order/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.techie.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // this is for feign client which is used to call other microservices
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
